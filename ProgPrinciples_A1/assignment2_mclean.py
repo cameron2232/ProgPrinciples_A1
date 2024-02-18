ed734b22364e6e684f338c252bbd9625dedee035
Cameron <mcleancam22@gmail.com>
2024-02-18 13:03:18 -0500

    Assignment 2 done

--- ProgPrinciples_A1/assignment2_mclean.py
+++ ProgPrinciples_A1/assignment2_mclean.py
@@ -1,10 +1,101 @@
+# Name: Cameron McLean
+# Class: PROG10004 20230905_00
+# Assignment: Assignment #2
+# Date: February 18, 2024
+# Program: assignment2_mclean.py
+# Description: A program that takes the number of times the user wishes to roll
+# a dice, rolls said dice the provided amount of times, and prints out the
+# count of each side rolled and a corresponding visual respresentation
+
 import assignment2_mclean_helpers
+import math
 
 def main():
+    # while True loop, loops indefinitely until 0 is inputted
+    while True:
+        
+        # Calls rolling time function, receving a valid input from the user
+        # of how many times to roll the dice
+        rolling_times = assignment2_mclean_helpers.get_rolls(1, 100000,
+            'Rolling Times(1, 10000, 0 to stop): ', 'Invalid number')
     
-    dice_one, dice_two, dice_three, dice_four, dice_five,  dice_six = assignment2_mclean_helpers.dice_rolling(assignment2_mclean_helpers.get_rolls(1, 100000, 'Rolling Times(1, 10000, 0 to stop): ', 'Invalid number'))
+        # Ends program if user inputs 0 in rolling_times function
+        if rolling_times == 0:
+            return None
+          
+        # Calls dice_rolling function, simulating dice rolls for count
+        # specified by rolling_times, and assigning the amount of times each
+        # face was rolled into each corresponding variable
+        dice_one, dice_two, dice_three, dice_four, dice_five, dice_six \
+          = assignment2_mclean_helpers.dice_rolling(rolling_times)
+ 
+        # Goes through checking amount each side was rolled, and setting
+        # largest_dice to the largest number
+        largest_dice = dice_one
+        if largest_dice < dice_two: largest_dice = dice_two
+        if largest_dice < dice_three: largest_dice = dice_three
+        if largest_dice < dice_four: largest_dice = dice_four
+        if largest_dice < dice_five: largest_dice = dice_five
+        if largest_dice < dice_six: largest_dice = dice_six
+    
+        char_count = 0
 
-    assignment2_mclean_helpers.print_results(dice_one, dice_two, dice_three, dice_four, dice_five, dice_six)
-    pass
+        # Goes through each dice, finds the percentage difference compared
+        # to largest_dice, and rounds
+        if dice_one != 0:
+            dice_percentage = dice_one / largest_dice
+            char_count = 70 * dice_percentage
+            char_count = math.floor(char_count)
+      
+        # Calls print_results function, printing out a visual representation of
+        # the amount of times a side was rolled compared to other sides
+        # and repeat said process for each side of the dice           
+        assignment2_mclean_helpers.print_results('One  ', char_count, '=',
+            dice_one)
+    
+        if dice_two != 0:
+            dice_percentage = dice_two / largest_dice
+            char_count = 70 * dice_percentage
+            char_count = math.floor(char_count)
+       
+        assignment2_mclean_helpers.print_results('Two  ', char_count, '=',
+            dice_two)
+    
+        if dice_three != 0:
+            dice_percentage = dice_three / largest_dice
+            char_count = 70 * dice_percentage
+            char_count = math.floor(char_count)
+       
+        assignment2_mclean_helpers.print_results('Three', char_count, '=',
+            dice_three)
+    
+        if dice_four != 0:
+            dice_percentage = dice_four / largest_dice
+            char_count = 70 * dice_percentage
+            char_count = math.floor(char_count)
+        
+        assignment2_mclean_helpers.print_results('Four ', char_count, '=',
+            dice_four)
+    
+        if dice_five != 0:
+            dice_percentage = dice_five / largest_dice
+            char_count = 70 * dice_percentage
+            char_count = math.floor(char_count)
+        
+        assignment2_mclean_helpers.print_results('Five ', char_count, '=',
+            dice_five)
+    
+        if dice_six != 0:
+            dice_percentage = dice_six / largest_dice
+            char_count = 70 * dice_percentage
+            char_count = math.floor(char_count)
+
+        assignment2_mclean_helpers.print_results('Six  ', char_count, '=',
+            dice_six)
+        
+        print(), print()
+    
+    
+
 
 main()
--- ProgPrinciples_A1/assignment2_mclean_helpers.py
+++ ProgPrinciples_A1/assignment2_mclean_helpers.py
@@ -1,23 +1,48 @@
+# Name: Cameron McLean
+# Class: PROG10004 20230905_00
+# Assignment: Assignment #2
+# Date: February 18, 2024
+# Program: assignment2_mclean.py
+# Description: A program that takes the number of times the user wishes to roll
+# a dice, rolls said dice the provided amount of times, and prints out the
+# count of each side rolled and a corresponding visual respresentation
+
 import math
 import random
 
+# get_rolls function, which prompts the user to provide a valid input
+# of amount of rolls user would like to be performed, returning an error
+# message if inputted number does not fall between the upper and lower bound.
+# returns rolling_times if valid input is entered, and prints 'Thank you'
+# if user inputs 0.
 def get_rolls(lower_bound, upper_bound, prompt_message, error_message):
     
     rolling_times = 0
 
+    # while loop that will continue looping until a valid input is entered
     while rolling_times < lower_bound or rolling_times > upper_bound:
+        
+        # prompts user to input a number between provided bounds
         rolling_times = int(input(prompt_message))
+        
+        # prints 'Thank you' if user inputs 0
         if rolling_times == 0:
             print('Thank you')
             return rolling_times
+        
+        # prints an error message if user inputs invalid number
         elif rolling_times < lower_bound or rolling_times > upper_bound:
             print(error_message)
+        
+        # returns rolling_times if user input a valid number
         else: return rolling_times
                 
 def dice_rolling(rolling_times):
     
     dice_one = dice_two = dice_three = dice_four = dice_five = dice_six = 0
 
+    # rolls a random number between 1 and 6, and depending on the number 
+    # rolled, adds 1 to corresponding side of the dice variable
     for i in range(rolling_times):
         random_num = random.randint(1, 6)
         if random_num == 1: dice_one+= 1
@@ -26,12 +51,20 @@ def dice_rolling(rolling_times):
         elif random_num == 4: dice_four += 1
         elif random_num == 5: dice_five += 1
         elif random_num == 6: dice_six += 1
+        
+    # returns each dice side variable 
     return dice_one, dice_two, dice_three, dice_four, dice_five, dice_six
 
-def print_results(dice_one, dice_two, dice_three, dice_four, dice_five, dice_six):
-    print(dice_one)
-    print(dice_two)
-    print(dice_three)
-    print(dice_four)
-    print(dice_five)
-    print(dice_six)
+def print_results(dice_title, line_length, print_character, roll_count):
+   
+    # prints visual representation of the number of times said dice was rolled
+    # using the print_character and line_length as an indicator of the amount
+    # rolled compared to the dice with the highest number of rolls
+    print()
+    print(f'{dice_title}:  ', end = '')
+    if roll_count != 0:
+        for i in range(line_length):
+            print (print_character, end = '')
+    print(f' {roll_count}', end = '')
+    
+    
